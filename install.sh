0f84659d15fb2ece018f4362a35ec1127e1f877f
Enrique Giraldo <hgiraldos@gmail.com>
2017-11-03 20:50:32 +0100

    install.sh: update, improvements

--- install.sh
+++ install.sh
@@ -1,24 +1,28 @@
-#/bin/bash
-DEPENDENCIES="tmux vim zsh"
+#!/bin/bash
 
-for i in $DEPENDENCIES; do
-  type $i > /dev/null 2>&1 || {
-  echo "I requiere $DEPENDENCIES, at least $i i not installed"
-  }
-done
+check_dependencies() {
+  DEPEND_FAIL=0
+  echo "Dependencies:"
+  for i in $@; do
+    { type $i > /dev/null 2>&1 && echo "$i PASS"; } ||
+    {  echo "$i FAIL"; DEPEND_FAIL=1; }
+  done
 
-fmv() {
-  cp $2 $2.bk
-  rm $2
-#SELECT COPY OF LN
-  ln -s $1 $2
-#  cp $1 $2
+  if [ $DEPEND_FAIL -eq 1 ]; then
+    exit -1
+  fi
 }
 
-fmv _vimrc ~/.vimrc
-fmv _tmux.conf ~/.tmux.conf
-fmv _zshrc ~/.zshrc
-chmod +x sshh_script
+fmv() {
+  cp $2 $2.bk 2> /dev/null
+  rm $2 2> /dev/null
+  #SELECT COPY OF LN
+  ln -s $(realpath $1) $2
+  #  cp $1 $2
+}
+
+DEPENDENCIES="tmux vim zsh ctags"
+check_dependencies $DEPENDENCIES
 
 #change your default shell
 if [ $SHELL != /bin/zsh ]; then
@@ -26,7 +30,22 @@ if [ $SHELL != /bin/zsh ]; then
 fi
 
 if [ ! -d /usr/share/oh-my-zsh ]; then
+  if [ -d ~/.oh-my-zsh ]; then mv ~/.oh-my-zsh ~/.oh-my-zsh_old; fi
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-  cp ~/.oh-my-zsh /usr/share/oh-my-zsh
+  sudo cp -r ~/.oh-my-zsh /usr/share/oh-my-zsh
 fi
+
+if [ -d ~/.vim/bundle/Vundle.vim ]; then
+  rm -rf ~/.vim/bundle/Vundle.vim_old 2>/dev/null
+  mv ~/.vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim_old;
+fi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+fmv _vimrc ~/.vimrc
+fmv _tmux.conf ~/.tmux.conf
+fmv _zshrc ~/.zshrc
+chmod +x sshh_script
+
+vim +PluginInstall +qall
+
 
